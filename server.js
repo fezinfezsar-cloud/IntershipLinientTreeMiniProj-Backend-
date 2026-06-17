a7aa0ff6ea34992ae977d9dbcd16a8f58187ba6b
Mohammed Fezin <fezinfezsar@gmail.com>
2026-06-17 14:41:05 +0530

    refactor: update server configuration and routes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
-const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
+
+const habitRoutes = require("./routes/habitRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use("/users", userRoutes);
+app.use("/habits", habitRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
